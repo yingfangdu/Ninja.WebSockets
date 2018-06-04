1bc6e15ce4f9beb2361f840826bdbe2e241b31fb
Yvonne Du <yingfangdu@gmail.com>
2018-06-04 16:19:33 -0700

    add nuget package spec

--- Ninja.WebSockets.Net/Ninja.WebSockets.Net/Properties/AssemblyInfo.cs
+++ Ninja.WebSockets.Net/Ninja.WebSockets.Net/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ninja.WebSockets.Net")]
+[assembly: AssemblyTitle("Ninja.WebSockets.BAE")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ninja.WebSockets.Net")]
+[assembly: AssemblyProduct("Ninja.WebSockets.BAE")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
